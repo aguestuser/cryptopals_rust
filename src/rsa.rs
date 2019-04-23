bf67c538a489ad75ce9ef89d8f0ff53e1e9f96b4
aguestuser <aguestuser@riseup.net>
2019-04-23 02:58:58 -0400

    [5.39] add `rsa#gen_keypair`
    * uses `rsa#gen_distinct_primes` and `rsa#gen_coprime`
    * introduces `rsa#mod_inverse` and `rsa#egcd`

--- src/rsa.rs
+++ src/rsa.rs
@@ -1,14 +1,35 @@
-// extern crate num;
 extern crate num_bigint_dig as bigint;
 extern crate primal;
 extern crate rand;
 
-use num::integer::gcd;
-use num::traits::One;
-use bigint::{BigUint, RandBigInt};
+use bigint::{BigInt, BigUint, RandBigInt, ToBigInt};
 use bigint::prime;
+use num::integer::{lcm, gcd};
+use num::traits::{One,Zero};
 use rand::rngs::{OsRng};
 use rand::{CryptoRng};
+
+pub struct PublicKey {
+    pub e:    BigUint,
+    pub n:    BigUint,
+}
+
+pub struct PrivateKey {
+    pub d:    BigUint,
+    pub n:    BigUint,
+    lambda_n: BigUint,
+}
+
+pub fn gen_keypair(keysize: usize) -> (PublicKey, PrivateKey) {
+    let (p,q) = gen_distinct_primes(keysize);
+    let lambda_n = lcm(&p - BigUint::one(), &q - BigUint::one());
+    let e = gen_coprime(&lambda_n);
+    let d = mod_inverse(&e, &lambda_n).unwrap();
+    (
+        PublicKey { n: &p * &q, e },
+        PrivateKey { n: &p * &q, d, lambda_n }
+    )
+}
 
 pub fn gen_distinct_primes(keysize: usize) -> (BigUint, BigUint){
     let mut rng = OsRng::new().expect("Failed to build RNG");
@@ -20,14 +41,47 @@ fn gen_prime<T: CryptoRng + RandBigInt>(mut rng: T, keysize: usize) -> BigUint {
 }
 
 pub fn gen_coprime<'a>(x: &'a BigUint) -> BigUint {
-    let mut y = BigUint::new(vec![17]);
+    let mut y = BigUint::new(vec![65_537]);
     loop {
-        // TODO: how to avoid cloning here?
+        // TODO: possible to avoid cloning x and y here?
         if gcd(x.clone(),y.clone()).is_one() {
             break y
         } else {
-            y = prime::next_prime(&y);
+            y = y + BigUint::one();
         }
+    }
+}
+
+
+/// Calculates the modular multiplicative inverse *x* of an integer *a*,
+/// such that *ax* ≡ 1 (mod *m*), using the extended euclidian algorithm method.
+///
+/// Modified from: https://github.com/simon-andrews/rust-modinverse
+/// See also: https://en.wikipedia.org/wiki/Modular_multiplicative_inverse
+pub fn mod_inverse(a: &BigUint, m: &BigUint) -> Option<BigUint> {
+    let (a_, m_) = (a.to_bigint().unwrap(), m.to_bigint().unwrap());
+    let (g, x, _) = egcd(a_, m_.clone());
+    if g.is_one() {
+       Some((x % m_.clone() + m_.clone()) % m_.clone()).and_then(|bi| bi.to_biguint())
+    } else {
+       None
+    }
+
+}
+
+/// Implements the extended euclidian algorithm.
+///
+/// Ie: it finds the greatest common denominator of two integers *a* and *b*, and two
+/// integers *x* and *y* such that *ax* + *by* is the greatest common
+/// denominator of *a* and *b* (Bézout coefficients).
+///
+/// See: https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+pub fn egcd(a: BigInt, b: BigInt) -> (BigInt, BigInt, BigInt) {
+    if a.is_zero() {
+        (b, BigInt::zero(), BigInt::one())
+    } else {
+        let (g, x, y) = egcd(&b % &a, a.clone());
+        (g, y - ((b / a) * x.clone()), x.clone())
     }
 }
 
@@ -50,6 +104,13 @@ mod tests {
     use super::*;
 
     #[test]
+    fn generating_keypair(){
+        let (PublicKey{ e, n: n1 }, PrivateKey{ d, n: n2, lambda_n }) = gen_keypair(32);
+        assert_eq!(n1, n2);
+        assert_eq!((e * d) % lambda_n, BigUint::one());
+    }
+
+    #[test]
     fn generating_distinct_primes(){
         let (p1, q1) = gen_distinct_primes(32);
         let (p2, q2) = gen_distinct_primes(32);
@@ -67,8 +128,6 @@ mod tests {
         let coprime = gen_coprime(&int);
         assert_eq!(gcd(int, coprime), One::one());
     }
-
-
 
     #[test]
     fn encoding_byte_array_as_int() {
